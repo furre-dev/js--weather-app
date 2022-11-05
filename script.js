18d6bb6ddfe6031c3b093b799d168c85b0b6a4c9
Furkan Abay <furkan_abay@hotmail.com>
2022-11-05 02:42:40 +0100

    added geo api

--- script.js
+++ script.js
@@ -12,7 +12,12 @@ const cityInput = document.querySelector(".city-input");
 let cities_json_url = "./cities.json";
 
 //makes the first card always be stockholm!
-getWeatherData(card, 59.329, 18.068);
+getWeatherData(card, "Stockholm", 59.329, 18.068);
+
+
+
+
+
 
 function lowercase(text) {
   if (text.indexOf(" ") > 1) {
@@ -22,17 +27,19 @@ function lowercase(text) {
     let firstCapitalAfterSpace = text.charAt(indexOfSpace + 1).toUpperCase();
     let remainingText = text.slice(indexOfSpace + 2).toLowerCase();
 
-    console.log(indexOfSpace);
 
     string = firstCapital + lowerText + firstCapitalAfterSpace + remainingText;
+    console.log(string)
     return string;
   } else {
     let firstCapital = text.charAt(0).toUpperCase();
     let lowerText = text.slice(1).toLowerCase();
 
     string = firstCapital + lowerText;
+    console.log(string)
     return string;
   }
+
 }
 
 cityInput.addEventListener("keypress", function (e) {
@@ -41,23 +48,24 @@ cityInput.addEventListener("keypress", function (e) {
   }
 });
 
-async function returnLat(city_name) {
-  const response = await fetch(cities_json_url);
+async function returnLat(user_input) {
+  const geocode_url = `http://api.openweathermap.org/geo/1.0/direct?q=${user_input}&&appid=${"f58ad5e3a5e3480869752e3d146475aa"}`;
+  const response = await fetch(geocode_url);
   const data = await response.json();
 
   const allCards = document.querySelectorAll(".cards");
 
 
   let cityExists = false;
-   allCards.forEach((e) => {
-    if(e.children[0].innerHTML == city_name) {
-        console.log(e.children[0].innerHTML)
-        cityExists = true;
+  allCards.forEach((e) => {
+    if (e.children[0].innerHTML == user_input) {
+      console.log(e.children[0].innerHTML)
+      cityExists = true;
     }
   });
   console.log("city exists =" + cityExists)
-  if(cityExists === true) {
-    
+  if (cityExists === true) {
+
     return null;
   }
   console.log("city exists =" + cityExists)
@@ -65,35 +73,34 @@ async function returnLat(city_name) {
   let lat = 0;
   let long = 0;
 
-  data.city.forEach((element) => {
-    if (element.name == city_name) {
+
+  data.forEach((element) => {
+    if (element.name == user_input) {
       lat = element.lat;
-      long = element.long;
+      long = element.lon;
+
+      const cityName = element.name;
+
 
       const clone = card.cloneNode(true);
       cardContainer.appendChild(clone);
 
-      getWeatherData(clone, lat, long);
+      getWeatherData(clone, cityName, lat, long);
     }
   });
 }
 
-async function getWeatherData(element, lat, long) {
-  let weather_geocode_url = `http://api.openweathermap.org/geo/1.0/direct?q={city name}&limit={limit}&appid={API key}`
-  
-  
-  
-    let weather_api_url =
-  `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=f58ad5e3a5e3480869752e3d146475aa`;
+async function getWeatherData(element, city_name, lat, long) {
+  let weather_api_url =
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=f58ad5e3a5e3480869752e3d146475aa`;
   const response = await fetch(weather_api_url);
   const data = await response.json();
 
   const iconCode = data.weather[0].icon;
 
-console.log(data);
 
   //H3 city name
-  element.children[0].textContent = data.name;
+  element.children[0].textContent = city_name;
   //Temp
   element.children[1].textContent = Math.round(data.main.temp) + "°C";
   //Image
